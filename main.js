a8edf62b93cf7db939668ecdd041b832957049ec
Alexander-Pistun <110295928+Alexander-Pistun@users.noreply.github.com>
2022-09-18 11:02:10 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var name1="Alexander";
+var name2="Pistun";
+var full_name=name1.concat(name2);
+console.log (full_name);
+var a=44;
+var b=67
+var sum=a+b;
+console.log (sum);
+
+var sub=a-b;
+console.log (sub);
+
+var mul=a*b;
+console.log (mul);
+
+var div=a/b;
+console.log (div);
